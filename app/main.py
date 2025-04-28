fed47941eeb50541d4d9900d037b4d3cc6ee923b
Juan Sebastian Fernandez <juansf_b@hotmail.com>
2025-04-27 19:32:17 -0500

    Agregando requirements.txt 2

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import time
-from .Routers import users, movies, jwtoauth2
+from app.Routers import users, movies, jwtoauth2
 import uvicorn
 
 app = FastAPI(title="FastAPI - Movies API",
